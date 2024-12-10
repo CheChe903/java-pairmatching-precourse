8958a9a1393aed8f9b307570dde8d378dcee0205
CheChe903 <cheche903@naver.com>
2024-12-10 13:50:11 +0900

    feat: 초기 구현
    힌트 추가 및 컨트롤러, 예외 틀 추가합니다.

---
+++ src/main/java/pairmatching/utils/exception/ErrorMessage.java
@@ -0,0 +1,16 @@
+package pairmatching.utils.exception;
+
+public enum ErrorMessage {
+
+    private static final String PREFIX = "[ERROR] ";
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return PREFIX + message;
+    }
+}
